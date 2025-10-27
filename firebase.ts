7fa81fabc378269c7122a739038ebaeeb288bb96
MurilloSouza <95144657+MurilloSouza@users.noreply.github.com>
2025-10-27 10:58:22 +0000

    Corrige o projectId do Firebase para 'unnoorca'

--- firebase.ts
+++ firebase.ts
@@ -4,16 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKF2fJIcnTuX1tmdemS-qs21FenAHPkNs",
-  authDomain: "unnoorca.firebaseapp.com",
-  projectId: "unnoorca",
-  storageBucket: "unnoorca.firebasestorage.app",
-  messagingSenderId: "552320095943",
-  appId: "1:552320095943:web:cff8aa40c95ef9a886d0e2",
-  measurementId: "G-3QRRQH98G5"
+  apiKey: "AIzaSyBYmEDSoZ5vFwRvQw-flqlEBM6g2CU6-yc",
+  authDomain: "projeto-unno---orcamento.firebaseapp.com",
+  projectId: "projeto-unno---orcamento",
+  storageBucket: "projeto-unno---orcamento.firebasestorage.app",
+  messagingSenderId: "505424819431",
+  appId: "1:505424819431:web:3b9ab4831c73f2132b1e42",
+  measurementId: "G-0LB76VSR6G"
 };
 
               // Initialize Firebase
